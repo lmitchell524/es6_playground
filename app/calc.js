8f1205faeb0f4a80f5e186ad8396447bb23bdb52
lmitchell524 <lmitchell524@gmail.com>
2017-12-06 12:55:05 -0800

    created js and index files for import export practice - basis was doMath

---
+++ app/calc.js
@@ -0,0 +1,7 @@
+export default function(x, y){
+    return x + y;
+}
+
+export const subtract = (x, y) => x - y;
+
+export const multiply = (x, y) => x * y;
--- app/index.js
+++ app/index.js
@@ -1,1 +1,12 @@
-console.log('App working: open app/index.js to start exploring ES6');
+import addStuff, {subtract, multiply} from './calc';
+// import * as math from './calc';
+
+console.log('5 + 7 =', addStuff(5, 7));
+console.log('8 - 9 =', subtract(8, 9));
+console.log('4 * 3 =', multiply(4, 3));
+
+// console.log('math', math);
+//
+// console.log('5 + 7 =', math.default(5, 7));
+// console.log('8 - 9 =', math.subtract(8, 9));
+// console.log('4 * 3 =', math.multiply(4, 3));
